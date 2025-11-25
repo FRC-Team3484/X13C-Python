8ed23f5ee55739fc218d35266a9569a6be71d16c
thelegendofmario <ethanfrancis577@gmail.com>
2025-11-24 19:48:05 -0500

    initialized drivetrain subsystem

---
+++ src/constants.py
@@ -0,0 +1,5 @@
+class Drivetrain:
+    LEFT_MOTOR_PORT: int = 3
+    RIGHT_MOTOR_PORT: int = 2
+
+    RIGHT_INVERTED: bool = True
--- src/subsystems/drivetrain_subsystem.py
+++ src/subsystems/drivetrain_subsystem.py
@@ -1,2 +1,15 @@
 from wpilib.drive import DifferentialDrive
 from commands2 import Subsystem
+
+from phoenix5 import WPI_TalonSRX
+
+from src.constants import Drivetrain
+
+class DrivetrainSubsystem(Subsystem):
+    def __init__(self):
+        """
+        Drivetrain Subsystem base class
+        
+        Constants are pulled directly from the constants.py file.
+        """
+        pass
